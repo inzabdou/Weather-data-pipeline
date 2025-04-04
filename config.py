92c62776997b15232498c6b9fef97910e8bd26ac
inzabdou <abdouu.inz@gmail.com>
2025-04-04 19:04:56 +0200

    Updated ETL pipeline with Airflow automation

--- config.py
+++
@@ -1,4 +0,0 @@
-API_KEY = '779973fbe5b3f085052620a938ca5814'
-API_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
-REQUEST_CITY="London, uk"
-DB_PATH='weather_data.db'
--- loading.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-def load_to_sqlite(df, db_path):
-    conn = sqlite3.connect(db_path)
-    df.to_sql('weather', conn, if_exists='replace', index=False)
-    conn.close()
